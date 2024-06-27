e3d38adb5c5799613da9aa01681ed5e0f2fd90da
SamArtMediaDev <58868970+SamArtMediaDev@users.noreply.github.com>
2024-06-28 01:36:28 +0200

    Adding JS to the game

--- index.js
+++ index.js
@@ -1,27 +1,60 @@
+let scoreWon = 0;
+let scoreTie = 0;
+let scoreLose = 0;
+
+let gameEnded = false;
+
 function runGame(user_choice) {
+    if(gameEnded){
+        scoreWon = 0;
+        scoreTie = 0;
+        scoreLose = 0;
+        document.getElementById("gameEnd_img").src = "";
+        gameEnded = false;
+    }
+
     let cpu_choice = Math.floor(Math.random() * 3);
     
     if (cpu_choice === 0 ){
-        document.getElementById("h1-cpu").src = "assets/images/rock.png"
-        document.getElementById("h1-cpu").className = "rock"
+        document.getElementById("img-cpu").src = "assets/images/rock.png"
+        document.getElementById("img-cpu").className = "rock"
     }else if(cpu_choice === 1 ){
-        document.getElementById("h1-cpu").src = "assets/images/Paper.png"
-        document.getElementById("h1-cpu").className = "paper"
+        document.getElementById("img-cpu").src = "assets/images/Paper.png"
+        document.getElementById("img-cpu").className = "paper"
     }else {
-        document.getElementById("h1-cpu").src = "assets/images/scissors.png"
-        document.getElementById("h1-cpu").className = "scissor"
+        document.getElementById("img-cpu").src = "assets/images/scissors.png"
+        document.getElementById("img-cpu").className = "scissor"
     }
 
     if (user_choice === cpu_choice){
         document.getElementById("h1-result").innerHTML = "tie";
+        scoreTie += 1
     }else if(user_choice === 0 && cpu_choice === 1){
         document.getElementById("h1-result").innerHTML = "Lose";
+        scoreLose += 1
     }else if(user_choice === 1 && cpu_choice === 2){
         document.getElementById("h1-result").innerHTML = "Lose";
+        scoreLose += 1
     }else if(user_choice === 2 && cpu_choice === 0){
         document.getElementById("h1-result").innerHTML = "Lose";
+        scoreLose += 1
     }else {
         document.getElementById("h1-result").innerHTML = "Won"; 
+        scoreWon += 1;
     }
 
+    document.getElementById("score_won").innerHTML ="Won:" + scoreWon.toString();
+    document.getElementById("score_tie").innerHTML ="Tie:" + scoreTie.toString();
+    document.getElementById("score_lost").innerHTML="Lost:" + scoreLose.toString();
+
+    if(scoreWon === 6 && scoreLose < 6){
+        // you won
+        document.getElementById("gameEnd_img").src = "assets/images/champion-clipart.png";
+        gameEnded = true;
+        
+    }else if(scoreLose === 6 && scoreWon < 6){
+        // you lost
+        document.getElementById("gameEnd_img").src = "assets/images/youlose.png";
+        gameEnded = true;
+    }
 }
